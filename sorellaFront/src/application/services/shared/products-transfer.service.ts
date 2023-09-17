ce2802b2cd3c6ac9a2a0ac4cca62a60bf838f41b
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-17 07:02:16 -0500

    Fix a bug in the product transfer service

--- sorellaFront/src/application/services/shared/products-transfer.service.ts
+++ sorellaFront/src/application/services/shared/products-transfer.service.ts
@@ -8,10 +8,10 @@ import { AbstractProductsTransferService } from 'src/domain/interfaces/abstract-
 export class ProductsTransferService<T> implements AbstractProductsTransferService<T>{
 
   constructor() { }
-  override getProducts(): Observable<T> {
+  getProducts(): Observable<T> {
     throw new Error('Method not implemented.');
   }
-  override setProducts(text: T): void {
+  setProducts(text: T): void {
     throw new Error('Method not implemented.');
   }
 }
