561273c05693c168a0ec901482fd2210f9c2c5d6
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-17 20:39:42 -0500

    Delete some components

--- sorellaFront/src/app/tab2/tab2.module.ts
+++ sorellaFront/src/app/tab2/tab2.module.ts
@@ -6,12 +6,7 @@ import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { AbstractTextTransferService } from 'src/domain/interfaces';
-import { TextTransferService } from 'src/utilities/shared';
-import { AbstractProductsTransferService } from 'src/domain/interfaces/abstract-products-transfer.service'; 
-import { ProductsTransferService } from 'src/utilities/shared';
-import { ProductListComponent } from './components/product-list/product-list.component';
+
 
 @NgModule({
   imports: [
@@ -23,12 +18,8 @@ import { ProductListComponent } from './components/product-list/product-list.com
   ],
   declarations: [
     Tab2Page,
-    SearchBarComponent,
-    ProductListComponent
   ],
   providers: [
-    {provide:AbstractTextTransferService, useClass: TextTransferService},
-    {provide:AbstractProductsTransferService, useClass: ProductsTransferService}
   ]
 })
 export class Tab2PageModule {}
--- sorellaFront/src/app/tab2/tab2.page.ts
+++ sorellaFront/src/app/tab2/tab2.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  searchInput!: string;
 
   constructor() {}
 
+  onInputChange(){
+    
+  }
 }
