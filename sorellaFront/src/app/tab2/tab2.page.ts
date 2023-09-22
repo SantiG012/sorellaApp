b8e3106776b2c67e6a20414b3c7368077f3db6c2
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-22 11:24:35 -0500

    Fix the products grid

--- sorellaFront/src/app/tab2/tab2.page.ts
+++ sorellaFront/src/app/tab2/tab2.page.ts
@@ -13,6 +13,16 @@ export class Tab2Page {
   private searchTerms = new Subject<string>();
   productsNames$!:Observable<string[]>;
   products$!:Observable<Product[]>;
+  products = [{"_id":1,"productName":"Nick","productPrice":12.76,"productCategories":1,"status":false},
+  {"_id":2,"productName":"Clare","productPrice":44.74,"productCategories":2,"status":true},
+  {"_id":3,"productName":"Laurent","productPrice":94.29,"productCategories":3,"status":true},
+  {"_id":4,"productName":"Johnathon","productPrice":51.43,"productCategories":4,"status":true},
+  {"_id":5,"productName":"Kailey","productPrice":65.92,"productCategories":5,"status":false},
+  {"_id":6,"productName":"Nixie","productPrice":58.32,"productCategories":6,"status":false},
+  {"_id":7,"productName":"Ignacio","productPrice":79.38,"productCategories":7,"status":false},
+  {"_id":8,"productName":"Merissa","productPrice":40.35,"productCategories":8,"status":false},
+  {"_id":9,"productName":"Woody","productPrice":18.45,"productCategories":9,"status":false},
+  {"_id":10,"productName":"Opal","productPrice":33.15,"productCategories":10,"status":true}]
 
   constructor(
     private productService: AbstractProductService
