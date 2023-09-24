b98e1514ddba31da02d222b7d9c7fad6d605f6b9
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-24 12:30:01 -0500

    Add the dinamyc name-request

--- sorellaFront/src/app/tab2/tab2.page.ts
+++ sorellaFront/src/app/tab2/tab2.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { Observable, Subject, debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs';
 import { AbstractProductService } from 'src/application/abstractions';
 import { Product } from 'src/domain/models';
+import { CapitalizerUtil } from 'src/utils';
 
 @Component({
   selector: 'app-tab2',
@@ -13,16 +14,6 @@ export class Tab2Page {
   private searchTerms = new Subject<string>();
   productsNames$!:Observable<string[]>;
   products$!:Observable<Product[]>;
-  products = [{"_id":1,"productName":"Nick","productPrice":12.76,"productCategories":1,"status":false},
-  {"_id":2,"productName":"Clare","productPrice":44.74,"productCategories":2,"status":true},
-  {"_id":3,"productName":"Laurent","productPrice":94.29,"productCategories":3,"status":true},
-  {"_id":4,"productName":"Johnathon","productPrice":51.43,"productCategories":4,"status":true},
-  {"_id":5,"productName":"Kailey","productPrice":65.92,"productCategories":5,"status":false},
-  {"_id":6,"productName":"Nixie","productPrice":58.32,"productCategories":6,"status":false},
-  {"_id":7,"productName":"Ignacio","productPrice":79.38,"productCategories":7,"status":false},
-  {"_id":8,"productName":"Merissa","productPrice":40.35,"productCategories":8,"status":false},
-  {"_id":9,"productName":"Woody","productPrice":18.45,"productCategories":9,"status":false},
-  {"_id":10,"productName":"Opal","productPrice":33.15,"productCategories":10,"status":true}]
 
   constructor(
     private productService: AbstractProductService
@@ -32,7 +23,10 @@ export class Tab2Page {
     this.productsNames$ = this.searchTerms.pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap((term: string) => this.productService.getProductsNamesByFilterName(term))
+      switchMap((term: string) => this.productService.getProductsByName(term)),
+      map((products:Product[]) => products.map(product => {
+        return CapitalizerUtil.capitalize(product.nombre.toLocaleLowerCase());
+      }))
     );
   }
 
