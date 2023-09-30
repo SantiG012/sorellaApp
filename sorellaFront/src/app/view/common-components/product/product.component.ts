d09dde8180d5aa064eeabcadf0ff32815fa3074f
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-30 08:09:25 -0500

    Add a common component

---
+++ sorellaFront/src/app/view/common-components/product/product.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, OnInit, Output, TemplateRef } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
+})
+export class ProductComponent<T>  implements OnInit {
+  @Input() 
+  product!: T;
+
+  @Input()
+  productTemplate?:TemplateRef<any>;
+
+  @Output()
+  clickedProduct = new EventEmitter<T>();
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
