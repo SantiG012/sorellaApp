e1085dccd20b8c122a17ddcfefacb984eb55bdd0
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-30 07:28:49 -0500

    Relocate modules

--- sorellaFront/src/app/app-routing.module.ts
+++ sorellaFront/src/app/app-routing.module.ts
@@ -8,11 +8,11 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./view/login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: 'signup',
-    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+    loadChildren: () => import('./view/signup/signup.module').then( m => m.SignupPageModule)
   }
 ];
 @NgModule({
