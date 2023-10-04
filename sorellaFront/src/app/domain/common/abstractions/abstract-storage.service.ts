201bbb3ab412546c7901443aef490b0486e72e91
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-10-04 18:29:07 -0500

    Add common abstractions

--- sorellaFront/src/app/domain/common/abstractions/abstract-storage.service.ts
+++
@@ -1,5 +0,0 @@
-export abstract class AbstractStorageService<T> {
-    abstract get(key: string): Promise<T>;
-    abstract set(key: string, value:any): Promise<void>;
-    abstract remove(key: string):Promise<void>;
-}
--- sorellaFront/src/app/domain/common/mappers/mapper.ts
+++
@@ -1,4 +0,0 @@
-export abstract class Mapper<T, U> {
-  abstract mapFrom(param: T): U;
-  abstract mapTo(param: U): T;
-}
