9babb020139f6c0c967c700a9e24f54d2d3d2857
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-10-04 14:45:24 -0500

    Add a new storage service

---
+++ sorellaFront/src/app/domain/services/storage/abstract-product-storage.service.ts
@@ -0,0 +1,6 @@
+import { AbstractStorageService } from "../../common/abstractions";
+import { Product } from "../../models";
+
+export abstract class AbstractProductStorageService extends AbstractStorageService<Product> {
+
+}
