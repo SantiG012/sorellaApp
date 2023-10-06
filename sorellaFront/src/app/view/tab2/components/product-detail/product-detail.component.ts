02553db5720671bbde50212dbc20bc05a3b5821f
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-10-06 08:11:28 -0500

    Style the product detail component

--- sorellaFront/src/app/view/tab2/components/product-detail/product-detail.component.ts
+++ sorellaFront/src/app/view/tab2/components/product-detail/product-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, catchError, tap } from 'rxjs';
+import { AbstractProductService } from 'src/app/application/abstractions';
+import { Product } from 'src/app/domain/models';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,11 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent  implements OnInit {
+  isProductLoaded: boolean = false;
+  product!: Product;
+  selectedImageIndex: number = 0;
 
-  constructor() { }
+  constructor(
+    private readonly productService: AbstractProductService,
+    private readonly router: ActivatedRoute,
+    private readonly location: Location
+  ) { }
 
   ngOnInit() {
-    console.log('ProductDetailComponent ngOnInit');
+    const id = this.router.snapshot.paramMap.get('id');
+    this.productService.getProductById(id!).pipe(
+      tap(product => {
+        product.imagenes = product.imagenes.sort((a, b) => a.posicion - b.posicion);
+        this.product = product;
+        this.isProductLoaded = true;
+      }),
+      catchError(err => {
+        console.error("Mierda hijueputa",err);
+        return new Observable<Product>();
+      })
+    ).subscribe();
+  }
+
+  selectImage(index: number):void {
+    this.selectedImageIndex = index;
+  }
+
+  isSelectedImage(index: number): boolean {
+    return this.selectedImageIndex === index;
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
