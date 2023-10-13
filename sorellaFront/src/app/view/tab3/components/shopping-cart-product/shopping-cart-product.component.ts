d942aa8303f66afb9decf2715cc020da6c2744e1
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-10-13 05:56:35 -0500

    Implement the car product component

--- sorellaFront/src/app/view/tab3/components/shopping-cart-product/shopping-cart-product.component.ts
+++ sorellaFront/src/app/view/tab3/components/shopping-cart-product/shopping-cart-product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CarProductDto } from 'src/app/domain/DTO/product';
 
 @Component({
   selector: 'app-shopping-cart-product',
@@ -6,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart-product.component.scss'],
 })
 export class ShoppingCartProductComponent  implements OnInit {
+  @Input() cartProductDto!: CarProductDto;
 
   constructor() { }
 
   ngOnInit() {}
+
+  addUnit():void {
+    this.cartProductDto.cantidad!++;
+  }
+
+  removeUnit():void {
+    if(!this.isUnitValid()){return;}
+
+    this.cartProductDto.cantidad!--;
+  }
+
+  private isUnitValid():boolean {
+    return this.cartProductDto.cantidad! > 1;
+  }
 
 }
