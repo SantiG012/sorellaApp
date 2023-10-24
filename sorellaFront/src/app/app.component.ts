08f38c46c25f98df385e50196c32bf1a3e528800
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-10-24 17:59:28 -0500

    Implement deep linking for product detail page

--- sorellaFront/src/app/app.component.ts
+++ sorellaFront/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,NgZone } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProductStorageService } from './application/services/product-storage.service';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,31 @@ import { ProductStorageService } from './application/services/product-storage.se
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private productStorageService: ProductStorageService) {}
+  constructor(private productStorageService: ProductStorageService,
+    private ngZone: NgZone,
+    private router: Router
+    ) {}
+
+  ngOnInit() {
+    this.initializeApp();
+  }
+  
+  initializeApp() {
+    App.addListener('appUrlOpen', (data: URLOpenListenerEvent) => {
+      this.ngZone.run(() => {
+        const slug = this.extractSlug(data.url);
+        if (slug) {
+          this.navigateToslug(slug);
+        }
+      });
+    });
+  }
+
+  private extractSlug(url: string): string {
+    return url.split('.app').pop()!;
+  }
+
+  private navigateToslug(slug: string) {
+    this.router.navigateByUrl(slug);
+  }
 }
